4d438b8c6a5fb6614be9b84127a9f12ee6b87f38
Marten Schiwek <marten.schiwek@sap.com>
2025-01-03 12:52:38 +0100

    Adjustments for discovery

--- test/retention/discovery.test.js
+++ test/retention/discovery.test.js
@@ -10,6 +10,41 @@ describe('iLMObject discovery', () => {
         expect(data.length).toBeGreaterThan(0);
     });
 
+    test('Selection criteria are correctly determined', async () => {
+        const {status, data} = await GET('/drm/iLMObjects', { auth: DPI_Service });
+        expect(status).toEqual(200);
+        for (const iLMObject of data) {
+            for (const selectionCriteria of iLMObject.archivingConfiguration.selectionCriteria.concat(iLMObject.destructionConfiguration.selectionCriteria)) {
+                if (selectionCriteria.selectionCriteriaValueHelpEndPoint) {
+                    const {status, data} = await GET(selectionCriteria.selectionCriteriaValueHelpEndPoint, { auth: DPI_Service })
+                    expect(status).toEqual(200);
+                    expect(data.length).toBeGreaterThan(0)
+                    expect(data[0]).toMatchObject({
+                        value: expect.any(String),
+                        valueDescription: expect.any(String)
+                    })
+                }
+            }
+        }
+    });
+
+    test('Conditions are correctly determined', async () => {
+        const {status, data} = await GET('/drm/iLMObjects', { auth: DPI_Service });
+        expect(status).toEqual(200);
+        for (const iLMObject of data) {
+            for (const condition of iLMObject.conditions) {
+                expect(cds.model.definitions[`DRMService.${iLMObject.iLMObjectName}`].elements[condition.conditionFieldName]['@PersonalData.FieldSemantics']).toBeFalsy();
+                const {status, data} = await GET(condition.conditionFieldValueHelpEndPoint, { auth: DPI_Service })
+                expect(status).toEqual(200);
+                expect(data.length).toBeGreaterThan(0)
+                expect(data[0]).toMatchObject({
+                    conditionFieldValue: expect.any(String),
+                    conditionFieldValueDescription: expect.any(String)
+                })
+            }
+        }
+    });
+
     test('test org attribute endpoints', async () => {
         const { getDPIentities } = require('../../lib/model/get-dpi-entities');
         const DRMSRV = await cds.connect.to('DRMService')
