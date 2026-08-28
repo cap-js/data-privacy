b1a64699fcb845e76ffe93c6e1e6caaacb528728
Tobias Schmidt <tobias.schmidt05@sap.com>
2026-08-28 14:16:31 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   projects: [
-        // Integration tests in tests/ folder - run serially
-        {
+    // Integration tests in tests/ folder - run serially
+    {
       displayName: 'DPI Integration Tests',
       testMatch: [
         '<rootDir>/tests/!(add|build)/**/*.test.js',
         '<rootDir>/tests/!(add|build)/**/*.spec.js',
       ],
       testTimeout: 40000,
+      hookTimeout: 40000,
       maxWorkers: 1, // Force serial execution for integration tests
       modulePathIgnorePatterns: [
         '<rootDir>/tests/incidents-mgmt/build',
@@ -15,13 +16,13 @@ module.exports = {
         '<rootDir>/tests/incidents-mgmt/app/router',
         '<rootDir>/tests/add',
         '<rootDir>/tests/build',
-            ],
-        },
-        // CDS ADD tests
-        {
+      ],
+    },
+    // CDS ADD tests
+    {
       displayName: 'CDS ADD',
       testMatch: ['<rootDir>/tests/add/*.test.js', '<rootDir>/tests/add/**/*.spec.js'
-            ],
+      ],
       testTimeout: 60000,
       maxWorkers: 1,
       testEnvironment: 'node',
@@ -31,9 +32,9 @@ module.exports = {
         '<rootDir>/tests/incidents-mgmt/app/router',
         '<rootDir>/tests/incidents-mgmt/mtx',
       ],
-        },
-        // CDS BUILD tests - increased timeout, no module resets (preserves cds.build)
-        {
+    },
+    // CDS BUILD tests - increased timeout, no module resets (preserves cds.build)
+    {
       displayName: 'CDS BUILD',
       testMatch: ['<rootDir>/tests/build/*.test.js'],
       testTimeout: 100000,
