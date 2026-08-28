aa40236e22fbdecab2d6d3a0628f68ce6043d550
Tobias Schmidt <tobias.schmidt05@sap.com>
2026-08-28 14:29:02 +0200

    adjusted formating

--- jest.config.js
+++ jest.config.js
@@ -3,49 +3,48 @@ module.exports = {
   projects: [
     // Integration tests in tests/ folder - run serially
     {
-      displayName: 'DPI Integration Tests',
+      displayName: "DPI Integration Tests",
       testMatch: [
-        '<rootDir>/tests/!(add|build)/**/*.test.js',
-        '<rootDir>/tests/!(add|build)/**/*.spec.js',
+        "<rootDir>/tests/!(add|build)/**/*.test.js",
+        "<rootDir>/tests/!(add|build)/**/*.spec.js"
       ],
       testTimeout: 40000,
       maxWorkers: 1, // Force serial execution for integration tests
       modulePathIgnorePatterns: [
-        '<rootDir>/tests/incidents-mgmt/build',
-        '<rootDir>/tests/bookshop-app/app/router',
-        '<rootDir>/tests/incidents-mgmt/app/router',
-        '<rootDir>/tests/add',
-        '<rootDir>/tests/build',
-      ],
+        "<rootDir>/tests/incidents-mgmt/build",
+        "<rootDir>/tests/bookshop-app/app/router",
+        "<rootDir>/tests/incidents-mgmt/app/router",
+        "<rootDir>/tests/add",
+        "<rootDir>/tests/build"
+      ]
     },
     // CDS ADD tests
     {
-      displayName: 'CDS ADD',
-      testMatch: ['<rootDir>/tests/add/*.test.js', '<rootDir>/tests/add/**/*.spec.js'
-      ],
+      displayName: "CDS ADD",
+      testMatch: ["<rootDir>/tests/add/*.test.js", "<rootDir>/tests/add/**/*.spec.js"],
       testTimeout: 60000,
       maxWorkers: 1,
-      testEnvironment: 'node',
+      testEnvironment: "node",
       modulePathIgnorePatterns: [
-        '<rootDir>/tests/incidents-mgmt/build',
-        '<rootDir>/tests/bookshop-app/app/router',
-        '<rootDir>/tests/incidents-mgmt/app/router',
-        '<rootDir>/tests/incidents-mgmt/mtx',
-      ],
+        "<rootDir>/tests/incidents-mgmt/build",
+        "<rootDir>/tests/bookshop-app/app/router",
+        "<rootDir>/tests/incidents-mgmt/app/router",
+        "<rootDir>/tests/incidents-mgmt/mtx"
+      ]
     },
     // CDS BUILD tests - increased timeout, no module resets (preserves cds.build)
     {
-      displayName: 'CDS BUILD',
-      testMatch: ['<rootDir>/tests/build/*.test.js'],
+      displayName: "CDS BUILD",
+      testMatch: ["<rootDir>/tests/build/*.test.js"],
       testTimeout: 100000,
       maxWorkers: 5,
-      testEnvironment: 'node',
+      testEnvironment: "node",
       modulePathIgnorePatterns: [
-        '<rootDir>/tests/incidents-mgmt/build',
-        '<rootDir>/tests/bookshop-app/app/router',
-        '<rootDir>/tests/incidents-mgmt/app/router',
-        '<rootDir>/tests/incidents-mgmt/mtx',
-      ],
+        "<rootDir>/tests/incidents-mgmt/build",
+        "<rootDir>/tests/bookshop-app/app/router",
+        "<rootDir>/tests/incidents-mgmt/app/router",
+        "<rootDir>/tests/incidents-mgmt/mtx"
+      ]
     }
-  ],
+  ]
 };
